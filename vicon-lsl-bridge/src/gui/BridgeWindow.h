b2d09ce171fc91964f41c4e8421d7594ec16912d
attackofthetitan <ethanlin.0902@gmail.com>
2026-05-27 16:06:55 +0800

    Add GUI gaze relay controls

--- vicon-lsl-bridge/src/gui/BridgeWindow.h
+++ vicon-lsl-bridge/src/gui/BridgeWindow.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <QWidget>
+#include <QCheckBox>
 #include <QLineEdit>
 #include <QPushButton>
 #include <QLabel>
+#include <QSpinBox>
 #include <QThread>
 #include <memory>
 #include "ViconLSLBridge.h"
@@ -15,8 +17,12 @@ public:
     void stopBridge();
 
 signals:
-    void statusUpdate(int state, size_t markers, size_t segments,
-                      unsigned int frames, const QString& message);
+    void statusUpdate(int state, unsigned long long markers, unsigned long long segments,
+                      unsigned int frames, bool gaze_enabled, bool gaze_listening,
+                      unsigned long long gaze_samples,
+                      unsigned long long gaze_malformed_packets,
+                      const QString& gaze_last_error,
+                      const QString& message);
 
 protected:
     void run() override;
@@ -35,20 +41,35 @@ public:
 private slots:
     void onStart();
     void onStop();
-    void onStatusUpdate(int state, size_t markers, size_t segments,
-                        unsigned int frames, const QString& message);
+    void onStatusUpdate(int state, unsigned long long markers, unsigned long long segments,
+                        unsigned int frames, bool gaze_enabled, bool gaze_listening,
+                        unsigned long long gaze_samples,
+                        unsigned long long gaze_malformed_packets,
+                        const QString& gaze_last_error,
+                        const QString& message);
     void onWorkerFinished();
 
 private:
+    void loadSettings();
+    void saveSettings() const;
+    void setInputsEnabled(bool enabled);
+
     QLineEdit* server_edit_;
     QLineEdit* marker_stream_edit_;
     QLineEdit* segment_stream_edit_;
+    QCheckBox* gaze_enabled_check_;
+    QSpinBox* gaze_port_spin_;
+    QLineEdit* gaze_stream_edit_;
     QPushButton* start_button_;
     QPushButton* stop_button_;
     QLabel* status_label_;
+    QLabel* gaze_status_label_;
     QLabel* markers_label_;
     QLabel* segments_label_;
     QLabel* frames_label_;
+    QLabel* gaze_samples_label_;
+    QLabel* malformed_packets_label_;
+    QLabel* last_error_label_;
 
     BridgeWorker* worker_ = nullptr;
 };
