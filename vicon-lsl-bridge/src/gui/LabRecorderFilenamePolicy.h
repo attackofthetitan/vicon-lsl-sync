fa5bf019a60f0efe930e6ad6931547099f61b233
attackofthetitan <ethanlin.0902@gmail.com>
2026-08-27 20:04:01 +0800

    Validate exact recording destinations

--- vicon-lsl-bridge/src/gui/LabRecorderFilenamePolicy.h
+++ vicon-lsl-bridge/src/gui/LabRecorderFilenamePolicy.h
@@ -2,6 +2,7 @@
 
 #include <QString>
 #include <QStringList>
+#include <QVector>
 
 struct LabRecorderFilenameFields {
     QString root;
@@ -14,6 +15,41 @@ struct LabRecorderFilenameFields {
     QString modality;
 };
 
+enum class RecordingPathIssueLevel {
+    Warning,
+    Error,
+};
+
+struct RecordingPathIssue {
+    RecordingPathIssueLevel level = RecordingPathIssueLevel::Error;
+    QString field;
+    QString message;
+    QString corrective_action;
+};
+
+struct RecordingPathValidationOptions {
+    bool allow_outside_study_root = false;
+    bool allow_overwrite = false;
+    bool create_parent_directories = false;
+    bool verify_write_access = true;
+    bool verify_storage = true;
+    qint64 storage_warning_bytes = 10LL * 1024LL * 1024LL * 1024LL;
+    int practical_path_length = 240;
+};
+
+struct RecordingPathResult {
+    LabRecorderFilenameFields normalized_fields;
+    QString relative_path;
+    QString absolute_path;
+    qint64 available_storage_bytes = -1;
+    QVector<RecordingPathIssue> issues;
+
+    bool valid() const;
+    bool hasWarnings() const;
+    QString firstError() const;
+    QString summary() const;
+};
+
 class LabRecorderFilenamePolicy {
 public:
     static QString filenameCommand(const LabRecorderFilenameFields& fields);
@@ -24,4 +60,10 @@ public:
     static QString sanitizedValue(QString value);
     static QString renderedFilenamePreview(const LabRecorderFilenameFields& fields);
     static QString validationError(const LabRecorderFilenameFields& fields);
+    static RecordingPathResult validate(
+        const LabRecorderFilenameFields& fields,
+        const RecordingPathValidationOptions& options = {});
+    static int findNextRun(const LabRecorderFilenameFields& fields,
+                           int current_run,
+                           const RecordingPathValidationOptions& options = {});
 };
